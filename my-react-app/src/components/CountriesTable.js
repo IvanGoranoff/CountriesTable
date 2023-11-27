507d5f27ffa871c5ff6b8723ec6d1e62b007ab48
IvanGoranov02 <ivangoranoff@gmail.com>
2023-11-27 02:29:31 +0200

    fix

--- my-react-app/src/components/CountriesTable.js
+++ my-react-app/src/components/CountriesTable.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination } from '@mui/material';
+import React, { useState, useMemo } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination, TableSortLabel } from '@mui/material';
 
 function CountriesTable({ countries }) {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
+
+    const getSortableValue = (country, key) => {
+        if (key === 'name') {
+            return country.name.common;
+        }
+        return country[key];
+    };
+
+    const sortedCountries = useMemo(() => {
+        let sortableCountries = [...countries];
+        if (sortConfig.key !== null) {
+            sortableCountries.sort((a, b) => {
+                const aValue = getSortableValue(a, sortConfig.key);
+                const bValue = getSortableValue(b, sortConfig.key);
+
+                if (aValue < bValue) {
+                    return sortConfig.direction === 'asc' ? -1 : 1;
+                }
+                if (aValue > bValue) {
+                    return sortConfig.direction === 'asc' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+        return sortableCountries;
+    }, [countries, sortConfig]);
+
+    const requestSort = (key) => {
+        const isAsc = sortConfig.key === key && sortConfig.direction === 'asc';
+        setSortConfig({ key, direction: isAsc ? 'desc' : 'asc' });
+    }
+
+    const createSortHandler = (property) => (event) => {
+        requestSort(property);
+    };
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
 
-    const handleChangeRowsPerPage = event => {
-        setRowsPerPage(+event.target.value);
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
 
@@ -20,17 +56,24 @@ function CountriesTable({ countries }) {
                 <Table stickyHeader aria-label="countries table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Флаг</TableCell>
-                            <TableCell>Име</TableCell>
-                            <TableCell>Столица</TableCell>
-                            <TableCell>Континент</TableCell>
-                            <TableCell>Валута</TableCell>
-                            <TableCell>CCA3</TableCell>
-                            <TableCell>Население</TableCell>
+                            {['flags', 'name', 'capital', 'continents', 'currencies', 'cca3', 'population'].map((headCell) => (
+                                <TableCell
+                                    key={headCell}
+                                    sortDirection={sortConfig.key === headCell ? sortConfig.direction : false}
+                                >
+                                    <TableSortLabel
+                                        active={sortConfig.key === headCell}
+                                        direction={sortConfig.key === headCell ? sortConfig.direction : 'asc'}
+                                        onClick={createSortHandler(headCell)}
+                                    >
+                                        {headCell}
+                                    </TableSortLabel>
+                                </TableCell>
+                            ))}
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {countries.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((country) => (
+                        {sortedCountries.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((country) => (
                             <TableRow key={country.cca3}>
                                 <TableCell component="th" scope="row">
                                     <img src={country.flags.svg} alt={`${country.name.common} flag`} style={{ width: '50px' }} />
